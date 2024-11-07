4739173c90fad5b55cb6afa8c963df8489ab0ee1
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-11-07 13:34:01 +0530

    chnages

--- landing/src/pages/Course.jsx
+++ landing/src/pages/Course.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Course_Banner from '../components/Course_Banner'
+import ContactUs from '../components/ContactUs'
 
 export default function Course() {
   return (
    <>
    <Course_Banner/>
+   <ContactUs/>
    </>
   )
 }
