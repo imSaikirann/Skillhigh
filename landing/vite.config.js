0e27681ff905b1cbf5b509af516375b77fd6b248
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-11-09 15:32:56 +0530

    chnages

--- landing/vite.config.js
+++ landing/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      css: {
+        // Add custom CSS preprocessor options if needed
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ['slick-carousel'], // Ensures slick-carousel is bundled correctly
+  },
 })
