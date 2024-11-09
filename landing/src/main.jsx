603b8b15bc59243c7dc8853297a257147e5b02ac
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-11-09 15:34:11 +0530

    chnages

--- landing/src/main.jsx
+++ landing/src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import '/node_modules/slick-carousel/slick/slick.css';
+import '/node_modules/slick-carousel/slick/slick-theme.css';
 
 
 
