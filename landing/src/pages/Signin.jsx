1ffd2cc9c75b1c2755b2361718da34c1cbdf5f8d
Sai Kiran <neelakantamsaikiran2@gmail.com>
2024-12-04 11:07:25 +0530

    changes

--- landing/src/pages/Signin.jsx
+++ landing/src/pages/Signin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState ,useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { auth, provider } from '../auth/firebase';
 import { signInWithPopup, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
